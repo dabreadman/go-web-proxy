b47bdbec80a2bbc936ff7a223bdd0d197faba8e1
dabreadman <tany3@tcd.ie>
2021-03-13 00:23:23 +0000

    HTTP works

--- proxy.go
+++ proxy.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"net/http"
 )
 
-var blockList map[string]bool
-var cache map[string]byte
+var blockList map[string]bool = make(map[string]bool)
+var cache map[string][]byte = make(map[string][]byte)
+var headerCache map[string]http.Header = make(map[string]http.Header)
 
 func httpsHandler(w http.ResponseWriter, req *http.Request) {
 	req.URL.Scheme = "https"
@@ -26,6 +28,7 @@ func httpsHandler(w http.ResponseWriter, req *http.Request) {
 
 	server := req.URL.Host
 	serverCon, err := net.Dial("tcp", server)
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -44,39 +47,54 @@ func httpsHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func httpHandler(w http.ResponseWriter, req *http.Request) {
-	return
-	// If request/response is cached
-	if cachedRes, ok := cache[req.URL.RawPath]; ok {
-		log.Printf("%s\n", string(cachedRes))
-		// If-Not-Modified request
-		// If 304{
-		//		Send cached response back to client
-		//		Close
-		// } else{
-		// 		delete(cache, url)
-		// }
-		//
-	}
-	log.Printf("http %s \n", req.Host)
-	// Make request
-	// Buffer response
-	// Make header
-	// Cache
-	/*
-		cache[url]=res
-		go func() {
-		    time.Sleep(90 * time.Second)
-		    delete(cache,res)
-		    }()
-	*/
 
-	// Send to client
-	resp, err := http.Get(req.URL.RawPath)
+	// If request/response is cached
+	// if cachedRes, ok := cache[req.URL.Host]; ok {
+	// 	log.Printf("%s\n", string(cachedRes))
+	// 	// If-Not-Modified request
+	// 	// If 304{
+	// 	//		Send cached response back to client
+	// 	//		Close
+	// 	// } else{
+	// 	// 		delete(cache, url)
+	// 	// }
+	// 	//
+	// }
+	// log.Printf("http %s \n", req.Host)
+	// // Make request
+	// // Buffer response
+	// // Make header
+	// // Cache
+	// /*
+	// 	cache[url]=res
+	// 	go func() {
+	// 	    time.Sleep(90 * time.Second)
+	// 	    delete(cache,res)
+	// 	    }()
+	// */
+
+	// // Send to client
+	resp, err := http.Get(req.RequestURI)
 	if err != nil {
 		log.Printf("%s\n", err)
 	}
-	log.Printf("%d", resp.StatusCode)
 	defer resp.Body.Close()
+	// Copies all headers
+	for k, v := range resp.Header {
+		for _, vv := range v {
+			w.Header().Set(k, vv)
+		}
+	}
+
+	// Copy body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+	}
+	cache[req.RequestURI] = body
+	headerCache[req.RequestURI] = resp.Header
+
+	fmt.Fprint(w, string(body))
 }
 
 func networkHandler(w http.ResponseWriter, req *http.Request) {
