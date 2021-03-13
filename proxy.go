e7ec3f6d3d978df0602268706a919c25fe174d0b
dabreadman <tany3@tcd.ie>
2021-03-13 16:14:31 +0000

    WIP

--- proxy.go
+++ proxy.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"log"
 	"net"
 	"net/http"
+	"os"
+	"strings"
+	"time"
 )
 
-var blockList map[string]bool = make(map[string]bool)
-var cache map[string][]byte = make(map[string][]byte)
-var headerCache map[string]http.Header = make(map[string]http.Header)
+var blockList map[string]bool = map[string]bool{
+	// "example.com": true,
+}
+
+var cache map[string]cacheItem = make(map[string]cacheItem)
+var CACHE_EXPIRY = 90 * time.Second
+
+// Struct to store cache with identifier
+type cacheItem struct {
+	body    []byte
+	headers http.Header
+	date    string
+}
 
 func httpsHandler(w http.ResponseWriter, req *http.Request) {
 	req.URL.Scheme = "https"
@@ -43,38 +57,43 @@ func httpsHandler(w http.ResponseWriter, req *http.Request) {
 
 	serverCon.Close()
 	clientCon.Close()
-
 }
 
 func httpHandler(w http.ResponseWriter, req *http.Request) {
 
+	URI := req.RequestURI
+	cachedRes, exist := cache[URI]
+	log.Printf("Cache Exist for URI %s? %t", URI, exist)
 	// If request/response is cached
-	// if cachedRes, ok := cache[req.URL.Host]; ok {
-	// 	log.Printf("%s\n", string(cachedRes))
-	// 	// If-Not-Modified request
-	// 	// If 304{
-	// 	//		Send cached response back to client
-	// 	//		Close
-	// 	// } else{
-	// 	// 		delete(cache, url)
-	// 	// }
-	// 	//
-	// }
-	// log.Printf("http %s \n", req.Host)
-	// // Make request
-	// // Buffer response
-	// // Make header
-	// // Cache
-	// /*
-	// 	cache[url]=res
-	// 	go func() {
-	// 	    time.Sleep(90 * time.Second)
-	// 	    delete(cache,res)
-	// 	    }()
-	// */
+	if exist {
+		// Add header to check if cache is fresh
+		location, _ := time.LoadLocation("GMT")
+		time := time.Now().In(location).Format(http.TimeFormat)
+		req.Header.Set("If-Modified-Since", time)
+		req.Header.Set("If-None-Match", cachedRes.headers.Get("Etag"))
+		log.Printf("Time: %v\n", time)
+		resp, err := http.Get(URI)
+		if err != nil {
+			log.Printf("%s\n", err)
+		}
+		defer resp.Body.Close()
+		// If not modified, use cache
+		log.Printf("%d %s", resp.StatusCode, URI)
+		//if resp.StatusCode == 200 {
+		log.Println("Retrieving from Cache")
+		for k, v := range cachedRes.headers {
+			for _, vv := range v {
+				w.Header().Set(k, vv)
+			}
+		}
+		fmt.Fprint(w, string(cachedRes.body))
+		log.Printf("Used cache, returning")
+		return
+		//}
+	}
 
-	// // Send to client
-	resp, err := http.Get(req.RequestURI)
+	// Send to client
+	resp, err := http.Get(URI)
 	if err != nil {
 		log.Printf("%s\n", err)
 	}
@@ -86,19 +105,33 @@ func httpHandler(w http.ResponseWriter, req *http.Request) {
 		}
 	}
 
-	// Copy body
+	// Copy body and pass to client
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Println(err)
 	}
-	cache[req.RequestURI] = body
-	headerCache[req.RequestURI] = resp.Header
-
 	fmt.Fprint(w, string(body))
+	// Cache and set cache expiry
+	respDate := resp.Header.Get("date")
+	cachedData := cacheItem{
+		body:    body,
+		headers: resp.Header,
+		date:    respDate,
+	}
+	cache[URI] = cachedData
+
+	go func(date string) {
+		time.Sleep(CACHE_EXPIRY)
+		if cache[URI].date != date {
+			delete(cache, URI)
+		}
+		log.Printf("Killing cache for %s at %s \n%v\n", URI, cachedData.date, cachedData.headers)
+	}(respDate)
 }
 
 func networkHandler(w http.ResponseWriter, req *http.Request) {
 	host := req.Host
+	log.Printf("Host: %s, %s", host, req.RequestURI)
 	// If not in blockList
 	if !blockList[host] {
 		// If HTTPS
@@ -116,7 +149,37 @@ func networkHandler(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func blockListHandler() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Web Proxy Console")
+	fmt.Println("---------------------")
+
+	for {
+		fmt.Print("-> ")
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			log.Println(err)
+		}
+		// convert CRLF to LF
+		text = strings.Replace(text, "\n", "", -1)
+		arguments := strings.Split(text, " ")
+		fmt.Printf("\nPrinting\n%v\n", arguments)
+		switch arguments[0] {
+		case "list":
+			log.Printf("%v\n", blockList)
+		case "block":
+			blockList[arguments[1]] = true
+		case "unblock":
+			delete(blockList, arguments[1])
+		default:
+			log.Println("# Wrong input: <block/unblock> <URI>", text)
+		}
+		log.Printf("Entered line: %s\n", text)
+	}
+}
+
 func main() {
+	go blockListHandler()
 	networkHandler := http.HandlerFunc(networkHandler)
 	// Create a thread of networkHandler for each connection
 	http.ListenAndServe(":8080", networkHandler)
